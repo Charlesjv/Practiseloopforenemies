7b9adfb76acc39647bfe6eba07953242efcac4e5
Charlesjv <charles764jaison@gmail.com>
2019-10-04 23:37:31 -0400

    Aligning Enemy Gangs to the larger enemy Beetle

--- FInalAssignment/app/src/main/java/com/charlie/chippy/GameEngine.java
+++ FInalAssignment/app/src/main/java/com/charlie/chippy/GameEngine.java
@@ -16,7 +16,7 @@ import java.util.List;
 public class GameEngine extends SurfaceView implements Runnable {
 
 
-    final static String TAG="TAPPY-SPACESHIP";
+    final static String TAG="CHIPPY";
 
     // screen size
     int screenHeight;
@@ -54,9 +54,13 @@ public class GameEngine extends SurfaceView implements Runnable {
     EnemyGang enemyGang;
     EnemyGang enemyGang1;
     EnemyGang enemyGang2;
-    List<EnemyGang> enemyGangList = new ArrayList<EnemyGang>();
+    List<EnemyGang> enemyGangList1 = new ArrayList<EnemyGang>();
+    List<EnemyGang> enemyGangList2 = new ArrayList<EnemyGang>();
+    List<EnemyGang> enemyGangList3 = new ArrayList<EnemyGang>();
+    List<EnemyGang> enemyGangList4 = new ArrayList<EnemyGang>();
     List<Bullets> bulletsList = new ArrayList<Bullets>();
 
+    int BULLET_WIDTH = 35;
     // ----------------------------
     // ## GAME STATS
     // ----------------------------
@@ -85,8 +89,10 @@ public class GameEngine extends SurfaceView implements Runnable {
 
         this.spawnPlayer();
         this.spawnEnemies();
-        this.spawnEnemyGang();
-
+        this.spawnEnemyGang1();
+        this.spawnEnemyGang2();
+        this.spawnEnemyGang3();
+        this.spawnEnemyGang4();
         this.spawnBullets();
 
 
@@ -112,16 +118,43 @@ public class GameEngine extends SurfaceView implements Runnable {
 
     }
 
-    private void spawnEnemyGang(){
+    private void spawnEnemyGang1(){
+
+// Draw Enemy Gang on the right side of larger enemy Beetle
+        for(int i = 0; i<=6; i++) {
+
+            this.enemyGangList1.add(new EnemyGang(this.getContext(), enemy.getHitbox().right - 40, this.screenHeight / 2 - 400));
+        }
+
+    }
+    private void spawnEnemyGang2(){
+
+// Draw Enemy Gang on the bottom side of the larger enemy
+        for(int i = 0; i<=11; i++) {
+
+            this.enemyGangList2.add(new EnemyGang(this.getContext(), enemy.getBitmap().getWidth() , this.screenHeight / 2-140));
+        }
+
+    }
+
+    // Draw Enemy Gang on the left side of the larger enemy
+    private void spawnEnemyGang3(){
 
 
-        this.enemyGang = new EnemyGang(this.getContext(),enemy.getHitbox().right,this.screenHeight/2-400);
-        this.enemyGangList.add(new EnemyGang(this.getContext(),enemy.getHitbox().right ,this.screenHeight/2-315));
+        for(int i = 0; i<=6; i++) {
 
-        this.enemyGangList.add( new EnemyGang(this.getContext(),enemy.getHitbox().right ,this.screenHeight/2-235));
+            this.enemyGangList3.add(new EnemyGang(this.getContext(),  enemy.getBitmap().getWidth()/2 - 10 , this.screenHeight / 2 - 400));
+        }
 
-        this.enemyGangList.add(enemyGang);
+    }
+    // Draw Enemy Gang on the top side of the larger enemy
+    private void spawnEnemyGang4(){
 
+
+        for(int i = 0; i<=9; i++) {
+
+            this.enemyGangList4.add(new EnemyGang(this.getContext(), enemy.getBitmap().getWidth() +50, this.enemy.getBitmap().getHeight() - 60));
+        }
 
     }
 
@@ -168,10 +201,29 @@ public class GameEngine extends SurfaceView implements Runnable {
 
     int numloops = 0;
 
+    public void distance( double squarex, double squarey, double bulletx, double bullety){
+        double a = (squarex - bulletx);
+        double b = (squarey - bullety);
+        double distance = Math.sqrt((a*a) + (b*b));
+
+        // 2. calculate the "rate" to move
+        double xn = (a / distance);
+        double yn = (b / distance);
+
+        // 3. move the bullet
+       bulletx = bulletx + (int)(xn * 15);
+        bullety = bullety + (int)(yn * 15);
+
+        Log.d(TAG,"New bullet (x,y): (" + bulletx + "," + bullety + ")");
+
+    }
+
     public void updatePositions() {
 
 
         // Update position of background
+
+
 
 
         // @TODO: Update position of player
@@ -245,33 +297,124 @@ public class GameEngine extends SurfaceView implements Runnable {
             this.canvas.drawRect(enemyHitbox.left,enemyHitbox.top,enemyHitbox.right,enemyHitbox.bottom,paintbrush);
             this.canvas.drawBitmap(this.enemy.getBitmap(),this.enemy.getXPosition(),this.enemy.getYPosition(),paintbrush);
 
+//
+////            Rect  enemyGangHitbox = this.enemyGang.getHitbox();
+////            this.canvas.drawRect(enemyGangHitbox.left,enemyGangHitbox.top,enemyGangHitbox.right,enemyGangHitbox.bottom,paintbrush);
+//
+//            for(int i=0; i<enemyGangList.size();i++) {
+//
+//                Rect enemyGangHitbox = this.enemyGang.getHitbox();
+//
+//                int x = enemyGangList.get(i).xPosition;
+//                int y = enemyGangList.get(i).yPosition;
+//                canvas.drawRect(enemyGangList.get(i).getHitbox().left,enemyGangList.get(i).getHitbox().top,enemyGangList.get(i).getHitbox().right,enemyGangList.get(i).getHitbox().bottom,paintbrush);
+//
+//            }
+//            // enemyGangList
+//
+//            for(int i = 0; i < enemyGangList.size(); i++){
+//
+//                int x = enemyGangList.get(i).xPosition;
+//                int y = enemyGangList.get(i).yPosition;
+//
+//                canvas.drawBitmap(this.enemyGang.getBitmap(),x,y,paintbrush);
+//
+//            }
 
-//            Rect  enemyGangHitbox = this.enemyGang.getHitbox();
-//            this.canvas.drawRect(enemyGangHitbox.left,enemyGangHitbox.top,enemyGangHitbox.right,enemyGangHitbox.bottom,paintbrush);
+            paintbrush.setColor(Color.WHITE);
 
-            for(int i=0; i<enemyGangList.size();i++) {
+            // Drawing an array of enemyGang1 on the screen
 
-                Rect enemyGangHitbox = this.enemyGang.getHitbox();
+            for(int i = 1; i< enemyGangList1.size(); i++){
+                for(int j = 1; j< enemyGangList1.size(); j++){
+                    if(i == 1){
+                        this.canvas.drawBitmap(this.enemyGangList1.get(j).getBitmap(), this.enemyGangList1.get(j).getXPosition() + BULLET_WIDTH * j +10, this.enemyGangList1.get(j).getYPosition() + 10  , paintbrush);
+                    }
+                    this.canvas.drawBitmap(this.enemyGangList1.get(j).getBitmap(), this.enemyGangList1.get(j).getXPosition() + BULLET_WIDTH * j + 10, this.enemyGangList1.get(j).getYPosition() + BULLET_WIDTH * i  + 10, paintbrush);
 
-                int x = enemyGangList.get(i).xPosition;
-                int y = enemyGangList.get(i).yPosition;
-                canvas.drawRect(enemyGangList.get(i).getHitbox().left,enemyGangList.get(i).getHitbox().top,enemyGangList.get(i).getHitbox().right,enemyGangList.get(i).getHitbox().bottom,paintbrush);
-
+                }
             }
-            // enemyGangList
 
-            for(int i = 0; i < enemyGangList.size(); i++){
 
-                int x = enemyGangList.get(i).xPosition;
-                int y = enemyGangList.get(i).yPosition;
+            //Drawing an array of hitboxes to the enemyGangList1 on the screen
 
-                canvas.drawBitmap(this.enemyGang.getBitmap(),x,y,paintbrush);
-
+            for(int i = 1; i< enemyGangList1.size(); i++){
+                for (int j = 1; j<enemyGangList1.size(); j++){
+                    if(i == 1){
+                        this.canvas.drawRect(this.enemyGangList1.get(i).getHitbox().left + BULLET_WIDTH *j + 10,this.enemyGangList1.get(i).getHitbox().top,this.enemyGangList1.get(i).getHitbox().right + BULLET_WIDTH * j +10,this.enemyGangList1.get(i).getHitbox().bottom,paintbrush);
+                    }
+                    this.canvas.drawRect(this.enemyGangList1.get(i).getHitbox().left + BULLET_WIDTH *j + 10,this.enemyGangList1.get(i).getHitbox().top+ BULLET_WIDTH *i  + 10,this.enemyGangList1.get(i).getHitbox().right + BULLET_WIDTH * j +10,this.enemyGangList1.get(i).getHitbox().bottom+ BULLET_WIDTH * i + 10,paintbrush);
+                }
             }
 
 
+            // Drawing an array of enemyGang2 on the screen
 
+            for(int i = 1; i< enemyGangList2.size(); i++){
+                for(int j = 1; j< enemyGangList2.size(); j++){
+                    if(i == 1){
+                        this.canvas.drawBitmap(this.enemyGangList2.get(j).getBitmap(), this.enemyGangList2.get(j).getXPosition() + BULLET_WIDTH * j +10, this.enemyGangList2.get(j).getYPosition() + 10  , paintbrush);
+                    }
+                    this.canvas.drawBitmap(this.enemyGangList2.get(j).getBitmap(), this.enemyGangList2.get(j).getXPosition() + BULLET_WIDTH * j + 10, this.enemyGangList2.get(j).getYPosition() + BULLET_WIDTH * i  + 10, paintbrush);
 
+                }
+            }
+            // Drawing an array of hitboxes to the enemyGangList2 on the screen
+            for(int i = 1; i< enemyGangList2.size(); i++){
+                for (int j = 1; j<enemyGangList2.size(); j++){
+                    if(i == 1){
+                        this.canvas.drawRect(this.enemyGangList2.get(i).getHitbox().left + BULLET_WIDTH *j + 10,this.enemyGangList2.get(i).getHitbox().top,this.enemyGangList2.get(i).getHitbox().right + BULLET_WIDTH * j +10,this.enemyGangList2.get(i).getHitbox().bottom,paintbrush);
+                    }
+                    this.canvas.drawRect(this.enemyGangList2.get(i).getHitbox().left + BULLET_WIDTH *j + 10,this.enemyGangList2.get(i).getHitbox().top+ BULLET_WIDTH *i  + 10,this.enemyGangList2.get(i).getHitbox().right + BULLET_WIDTH * j +10,this.enemyGangList2.get(i).getHitbox().bottom+ BULLET_WIDTH * i + 10,paintbrush);
+                }
+            }
+
+            // Drawing an array of enemyGang3 on the screen
+
+            for(int i = 1; i< enemyGangList3.size(); i++){
+                for(int j = 1; j< enemyGangList3.size(); j++){
+                    if(i == 1){
+                        this.canvas.drawBitmap(this.enemyGangList3.get(j).getBitmap(), this.enemyGangList3.get(j).getXPosition() + BULLET_WIDTH * j +10, this.enemyGangList3.get(j).getYPosition() + 10  , paintbrush);
+                    }
+                    this.canvas.drawBitmap(this.enemyGangList3.get(j).getBitmap(), this.enemyGangList3.get(j).getXPosition() + BULLET_WIDTH * j + 10, this.enemyGangList3.get(j).getYPosition() + BULLET_WIDTH * i  + 10, paintbrush);
+
+                }
+            }
+
+            // Drawing an array of hitboxes to the enemyGangList3 on the screen
+
+            for(int i = 1; i< enemyGangList3.size(); i++){
+                for (int j = 1; j<enemyGangList3.size(); j++){
+                    if(i == 1){
+                        this.canvas.drawRect(this.enemyGangList3.get(i).getHitbox().left + BULLET_WIDTH *j + 10,this.enemyGangList3.get(i).getHitbox().top,this.enemyGangList3.get(i).getHitbox().right + BULLET_WIDTH * j +10,this.enemyGangList3.get(i).getHitbox().bottom,paintbrush);
+                    }
+                    this.canvas.drawRect(this.enemyGangList3.get(i).getHitbox().left + BULLET_WIDTH *j + 10,this.enemyGangList3.get(i).getHitbox().top+ BULLET_WIDTH *i  + 10,this.enemyGangList3.get(i).getHitbox().right + BULLET_WIDTH * j +10,this.enemyGangList3.get(i).getHitbox().bottom+ BULLET_WIDTH * i + 10,paintbrush);
+                }
+            }
+
+            // Drawing an array of enemyGang4 on the screeen
+
+            for(int i = 1; i< enemyGangList4.size(); i++){
+                for(int j = 1; j< enemyGangList4.size(); j++){
+                    if(i == 1){
+                        this.canvas.drawBitmap(this.enemyGangList4.get(j).getBitmap(), this.enemyGangList4.get(j).getXPosition() + BULLET_WIDTH * j +10, this.enemyGangList4.get(j).getYPosition() + 10  , paintbrush);
+                    }
+                    this.canvas.drawBitmap(this.enemyGangList4.get(j).getBitmap(), this.enemyGangList4.get(j).getXPosition() + BULLET_WIDTH * j + 10, this.enemyGangList4.get(j).getYPosition() + BULLET_WIDTH * i  + 10, paintbrush);
+
+                }
+            }
+            // Drawing an array of hitboxes to the enemyGangList4 on the screen
+
+            for(int i = 1; i< enemyGangList4.size(); i++){
+                for (int j = 1; j<enemyGangList4.size(); j++){
+                    if(i == 1){
+                        this.canvas.drawRect(this.enemyGangList4.get(i).getHitbox().left + BULLET_WIDTH *j + 10,this.enemyGangList4.get(i).getHitbox().top,this.enemyGangList4.get(i).getHitbox().right + BULLET_WIDTH * j +10,this.enemyGangList4.get(i).getHitbox().bottom,paintbrush);
+                    }
+                    this.canvas.drawRect(this.enemyGangList4.get(i).getHitbox().left + BULLET_WIDTH *j + 10,this.enemyGangList4.get(i).getHitbox().top+ BULLET_WIDTH *i  + 10,this.enemyGangList4.get(i).getHitbox().right + BULLET_WIDTH * j +10,this.enemyGangList4.get(i).getHitbox().bottom+ BULLET_WIDTH * i + 10,paintbrush);
+                }
+            }
+
+            paintbrush.setColor(Color.BLACK);
             //Draw bullets on the screen
 
             for(int i = 0; i < this.bulletsList.size(); i++ ){
